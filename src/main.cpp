c757baddb4f37b18cd153c7e4eaf587c4fbe7a23
Lukas Gygi <lukas.gygi@gmail.com>
2020-12-20 22:08:37 +0100

    updated to debounce lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "EEPROM.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Bounce2.h>
 #include <ESP32Encoder.h>
 #include <PID_v1.h>
 #include <SPI.h>
@@ -25,10 +26,8 @@
 #define ENCODER_RESOLUTION 2.0
 #define PID_ENCODER_RESOLUTION 100.0
 
-#define EEPROM_SIZE 16
+#define EEPROM_SIZE 1000
 int samples[NUMSAMPLES];
-
-float calibration = -2.3;
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -39,7 +38,8 @@ ESP32Encoder encoder;
 //Define Variables we'll be connecting to
 // output = active time of SSR / 10ms
 // 1 -> 10ms = 1 zero crossing
-double setpoint, input, output;
+double setpoint = 105, input, output;
+float calibration = -2.3;
 //Specify the links and initial tuning parameters
 double Kp = 2, Ki = 5, Kd = 1;
 PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
@@ -47,45 +47,28 @@ PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 int WindowSize = 500;
 unsigned long windowStartTime;
 
-volatile uint8_t state;
+uint8_t state;
 uint8_t previousState = 0;
-unsigned long previousPress;
-unsigned long pressTime = 0;
-int loopRuntime = 0;
-volatile bool longPress = false;
 
-uint8_t upper(float input)
-{
-    uint32_t temp = (uint32_t)setpoint;
-    return (byte)(temp >> 8);
-}
+bool longPress = false;
+
+Bounce b = Bounce();
 
 void saveValues()
 {
     //save values to eeprom, value * 100, then first lower byte, then upper
     // value * 1000 for pid values and calibration
-    EEPROM.write(0, (byte)(setpoint * 100));
-    EEPROM.write(1, upper(setpoint * 100));
-
-    EEPROM.write(2, (byte)(Kp * 1000));
-    EEPROM.write(3, upper(Kp * 1000));
-
-    EEPROM.write(4, (byte)(Ki * 1000));
-    EEPROM.write(5, upper(Ki * 1000));
-
-    EEPROM.write(6, (byte)(Kd * 1000));
-    EEPROM.write(7, upper(Kd * 1000));
-
-    EEPROM.write(8, (byte)(calibration * 1000));
-    EEPROM.write(9, upper(calibration * 1000));
+    int addr = 0;
+    EEPROM.writeFloat(addr, setpoint);
+    addr += sizeof(float);
+    EEPROM.writeFloat(addr, Kp);
+    addr += sizeof(float);
+    EEPROM.writeFloat(addr, Ki);
+    addr += sizeof(float);
+    EEPROM.writeFloat(addr, Kd);
+    addr += sizeof(float);
+    EEPROM.writeFloat(addr, calibration);
     EEPROM.commit();
-}
-
-float loadValues(uint8_t addr)
-{
-    float value = EEPROM.read(addr);
-    value += (uint32_t)(EEPROM.read(addr + 1) << 8);
-    return value;
 }
 
 // update the display output
@@ -96,82 +79,60 @@ void updateDisplay()
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
     display.print(input);
-    display.print(F(" C"));
+    display.print(F(" C "));
     display.setTextSize(1);
     display.print((int)output);
     display.println("%\n");
-    display.print(F("t:"));
     if (state == 0)
     {
         display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
     }
-    display.print(setpoint);
+    display.print(F("t:   "));
     display.setTextColor(SSD1306_WHITE);
-    display.print(F("  p:"));
+    display.println(setpoint);
     if (state == 1)
     {
         display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
     }
-    display.println(Kp);
+    display.print(F("p:   "));
     display.setTextColor(SSD1306_WHITE);
-    display.print(F("i:"));
+    display.println(Kp);
     if (state == 2)
     {
         display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
     }
-    display.print(Ki);
+    display.print(F("i:   "));
     display.setTextColor(SSD1306_WHITE);
-    display.print(F("  d:"));
+    display.println(Ki);
     if (state == 3)
     {
         display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
     }
-    display.println(Kd);
+    display.print(F("d:   "));
     display.setTextColor(SSD1306_WHITE);
-    display.print(F("cal:"));
+    display.println(Kd);
     if (state == 4)
     {
         display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
     }
+    display.print(F("cal: "));
+    display.setTextColor(SSD1306_WHITE);
     display.println(calibration);
     display.display();
 }
 
-void IRAM_ATTR encoderPress()
-{
-
-    if (digitalRead(ENCODER_BUTTON_PIN) == LOW)
-    {
-        if (millis() - previousPress > 100)
-        {
-            state = (state + 1) % 5;
-            pressTime = millis();
-        }
-        previousPress = millis();
-    }
-    if (digitalRead(ENCODER_BUTTON_PIN) == HIGH)
-    {
-        if (millis() - pressTime > 3000){
-            // long press
-            longPress = true;
-        }
-    }
-}
-
 void setup(void)
 {
-
     Serial.begin(115200);
     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
     pinMode(SSR_PIN, OUTPUT);
-
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
     {
         Serial.println(F("SSD1306 allocation failed"));
         // also probably set output low
         digitalWrite(SSR_PIN, LOW);
-        for (;;)
-            ; // Don't proceed, loop forever
+        delay(1000);
+        ESP.restart();
     }
 
     display.display();
@@ -185,12 +146,24 @@ void setup(void)
         display.println("faled to initialise EEPROM");
         delay(2000);
     }
-    setpoint = loadValues(0) / 100;
-    Kp = loadValues(2) / 1000;
-    Ki = loadValues(4) / 1000;
-    Kd = loadValues(6) / 1000;
-    calibration = loadValues(8) / 1000;
-
+    else
+    {
+        // load the values from EEPROM
+        for (int i = 0; i < 5; i += sizeof(float))
+        {
+            printf("EEPROM %d: %f\n", i, EEPROM.readFloat(i));
+        }
+        int addr = 0;
+        setpoint = EEPROM.readFloat(addr);
+        addr += sizeof(float);
+        Kp = EEPROM.readFloat(addr);
+        addr += sizeof(float);
+        Ki = EEPROM.readFloat(addr);
+        addr += sizeof(float);
+        Kd = EEPROM.readFloat(addr);
+        addr += sizeof(float);
+        calibration = EEPROM.readFloat(addr);
+    }
     // set initial PID values
     input = 25.0;
     myPID.SetOutputLimits(0, 100);
@@ -202,15 +175,25 @@ void setup(void)
     ESP32Encoder::useInternalWeakPullResistors = UP;
     // Attache pins for use as encoder pins
     encoder.attachHalfQuad(19, 18);
-    encoder.setCount(105 * ENCODER_RESOLUTION);
-    pinMode(ENCODER_BUTTON_PIN, INPUT_PULLUP);
-    attachInterrupt(ENCODER_BUTTON_PIN, encoderPress, CHANGE);
+    encoder.setCount(setpoint * ENCODER_RESOLUTION);
+    b.attach(ENCODER_BUTTON_PIN, INPUT_PULLUP);
+    b.interval(25);
 }
 
 void loop(void)
 {
-    unsigned long startTime = millis();
-    if (millis() % 20 == 0)
+    b.update();
+    bool changed = false;
+    if (b.changed())
+    {
+        if (b.read() == 0)
+        {
+            state = (state + 1) % 5;
+            longPress = true;
+            changed = true;
+        }
+    }
+    if (millis() % 50 == 0 || changed)
     {
 
         // measure temp
@@ -249,14 +232,14 @@ void loop(void)
             // roatary encoder controlls the setpoint
             if (previousState != 0)
             {
-                encoder.setCount(((int)setpoint * ENCODER_RESOLUTION));
+                encoder.setCount(setpoint * ENCODER_RESOLUTION);
             }
             setpoint = ((float)encoder.getCount()) / ENCODER_RESOLUTION;
             break;
         case 1:
             if (previousState != 1)
             {
-                encoder.setCount(((int)Kp * PID_ENCODER_RESOLUTION));
+                encoder.setCount(Kp * PID_ENCODER_RESOLUTION);
             }
             if (Kp != (((float)encoder.getCount()) / PID_ENCODER_RESOLUTION))
             {
@@ -267,7 +250,7 @@ void loop(void)
         case 2:
             if (previousState != 2)
             {
-                encoder.setCount(((int)Ki * PID_ENCODER_RESOLUTION));
+                encoder.setCount(Ki * PID_ENCODER_RESOLUTION);
             }
             if (Ki != (((float)encoder.getCount()) / PID_ENCODER_RESOLUTION))
             {
@@ -279,7 +262,7 @@ void loop(void)
         case 3:
             if (previousState != 3)
             {
-                encoder.setCount(((int)Kd * PID_ENCODER_RESOLUTION));
+                encoder.setCount(Kd * PID_ENCODER_RESOLUTION);
             }
             if (Kd != (((float)encoder.getCount()) / PID_ENCODER_RESOLUTION))
             {
@@ -290,7 +273,7 @@ void loop(void)
         case 4:
             if (previousState != 4)
             {
-                encoder.setCount(((int)calibration * 20.0));
+                encoder.setCount(calibration * 20.0);
             }
             calibration = ((float)encoder.getCount()) / 20.0;
             break;
@@ -299,7 +282,6 @@ void loop(void)
         input = steinhart;
         myPID.Compute();
         updateDisplay();
-        loopRuntime = startTime - millis();
     }
     unsigned long curTime = millis();
 
@@ -314,12 +296,18 @@ void loop(void)
         digitalWrite(SSR_PIN, HIGH);
     else
         digitalWrite(SSR_PIN, LOW);
-    
+
     // check for longpress and save to EEPROM if true
-    if (longPress)
+    if (b.read() == 0 && b.duration() > 3000 && longPress)
     {
         saveValues();
         display.writeFillRect(0, 0, 128, 64, SSD1306_WHITE);
         display.display();
+        longPress = false;
+
+        for (int i = 0; i < 16; i++)
+        {
+            printf("EEPROM %d: %d\n", i, EEPROM.read(i));
+        }
     }
 }
