dfabfe820b64479563e9539c5b8e4d4b6d6b68cc
Lukas Gygi <lukas.gygi@gmail.com>
2020-12-20 16:15:24 +0100

    set pid values while running, added crude UI

--- EspressoPID.ino
+++ EspressoPID.ino
@@ -8,6 +8,7 @@
 // which analog pin to connect
 #define THERMISTORPIN 4     
 #define SSR_PIN 2
+#define ENCODER_BUTTON_PIN 5
 // resistance at 25 degrees C
 #define THERMISTORNOMINAL 100000      
 // temp. for nominal resistance (almost always 25 C)
@@ -21,9 +22,10 @@
 #define SERIESRESISTOR 99000
 // how many speps per degree
 #define ENCODER_RESOLUTION 2.0
+#define PID_ENCODER_RESOLUTION 100.0
 int samples[NUMSAMPLES];
 
-const static float CALIBRATION = -2.3;
+float calibration = -2.3;
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -41,40 +43,69 @@ PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 // how long is one period of SSR control in ms
 int WindowSize = 500;
 unsigned long windowStartTime;
-//timer stuff
-//hw_timer_t *timer = NULL;
-//portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-//volatile uint8_t interruptCounter = 0;
+
+volatile uint8_t state;
+uint8_t previousState = 0;
+unsigned long previousPress;
 
 // update the display output
 void updateDisplay() {
   display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
   display.setCursor(0,0);
   display.print(input);
-  display.println(F(" C"));
-  display.print(F("set:"));
-  display.println(setpoint);
-  display.print(F("PID:"));
-  display.print(output);
+  display.print(F(" C"));
+  display.setTextSize(1);
+  display.print((int)output);
+  display.println("%\n");
+  display.print(F("t:"));
+  if (state == 0){
+    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+  }
+  display.print(setpoint);
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("  p:"));
+  if (state == 1){
+    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+  }
+  display.println(Kp);
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("i:"));
+  if (state == 2){
+    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+  }
+  display.print(Ki);
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("  d:"));
+  if (state == 3){
+    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+  }
+  display.println(Kd);
+  display.setTextColor(SSD1306_WHITE);
+  display.print(F("cal:"));
+  if (state == 4){
+    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+  }
+  display.println(calibration);
   display.display();
 }
 
-//// set the output appropriately
-//void IRAM_ATTR onTimer() {
-////  portENTER_CRITICAL_ISR(&timerMux);
-////  interruptCounter++;
-////  portEXIT_CRITICAL_ISR(&timerMux);
-//  if interruptCounter == threshold {
-//    
-//  }
-// 
-//}
+void IRAM_ATTR encoderPress() {
+    
+    if (millis() - previousPress > 200){
+      state = (state + 1) % 5;
+    }
+    previousPress = millis();
+
+}
 
 void setup(void) {
   
   Serial.begin(115200);
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   pinMode(SSR_PIN, OUTPUT);
+
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     // also probably set output low
@@ -92,13 +123,6 @@ void setup(void) {
   myPID.SetOutputLimits(0, 100);
   myPID.SetMode(AUTOMATIC);
   analogSetAttenuation(ADC_11db);
-  //start the timer (timer freq = 80Mhz -> 2nd arg prescaler)
-  //1us per timer tick
-//  timer = timerBegin(0, 80, true);
-//  timerAttachInterrupt(timer, &onTimer, true);
-//  // alarm freq = 100hz
-//  timerAlarmWrite(timer, 10000, true);
-//  timerAlarmEnable(timer);
 
   // encoder stuff
   // Enable the weak pull up resistors
@@ -106,48 +130,94 @@ void setup(void) {
   // Attache pins for use as encoder pins
   encoder.attachHalfQuad(19, 18);
   encoder.setCount(105 * ENCODER_RESOLUTION);
+  pinMode(ENCODER_BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(ENCODER_BUTTON_PIN, encoderPress, FALLING);
 }
  
 void loop(void) {
-
-  // measure temp
-  uint8_t i;
-  float average;
- 
-  // take N samples in a row, with a slight delay
-  for (i=0; i< NUMSAMPLES; i++) {
-   samples[i] = analogRead(THERMISTORPIN);
-  }
-  // average all the samples out
-  average = 0;
-  for (i=0; i< NUMSAMPLES; i++) {
-     average += samples[i];
-  }
-  average /= NUMSAMPLES;
-  // convert the value to resistance
-  average = 4095 / average - 1;
-  average = SERIESRESISTOR / average;
-  // calculate actual temp
-  float steinhart;
-  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-  steinhart = 1.0 / steinhart;                 // Invert
-  steinhart -= 273.15;           // convert absolute temp to C
-  steinhart += CALIBRATION;
-  // do the update
-  setpoint = ((float)encoder.getCount())/ENCODER_RESOLUTION;
-  input = steinhart;
-  myPID.Compute();
   if (millis() % 20 == 0){
+    // measure temp
+    uint8_t i;
+    float average;
+   
+    // take N samples in a row, with a slight delay
+    for (i=0; i< NUMSAMPLES; i++) {
+     samples[i] = analogRead(THERMISTORPIN);
+    }
+    // average all the samples out
+    average = 0;
+    for (i=0; i< NUMSAMPLES; i++) {
+       average += samples[i];
+    }
+    average /= NUMSAMPLES;
+    // convert the value to resistance
+    average = 4095 / average - 1;
+    average = SERIESRESISTOR / average;
+    // calculate actual temp
+    float steinhart;
+    steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+    steinhart = log(steinhart);                  // ln(R/Ro)
+    steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+    steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+    steinhart = 1.0 / steinhart;                 // Invert
+    steinhart -= 273.15;           // convert absolute temp to C
+    steinhart += calibration;
+    
+    // do the update for the FSM
+    switch (state) {
+      case 0:
+        // roatary encoder controlls the setpoint
+        if(previousState != 0){
+          encoder.setCount(((int)setpoint * ENCODER_RESOLUTION));
+        }
+        setpoint = ((float)encoder.getCount())/ENCODER_RESOLUTION;
+        break;
+      case 1:
+        if(previousState != 1){
+          encoder.setCount(((int)Kp * PID_ENCODER_RESOLUTION));
+        }
+        if (Kp != (((float)encoder.getCount())/PID_ENCODER_RESOLUTION)) {
+          Kp = ((float)encoder.getCount())/PID_ENCODER_RESOLUTION;
+          myPID.SetTunings(Kp, Ki, Kd);
+        }
+        break;
+      case 2:
+        if(previousState != 2){
+          encoder.setCount(((int)Ki * PID_ENCODER_RESOLUTION));
+        }
+        if (Ki != (((float)encoder.getCount())/PID_ENCODER_RESOLUTION)) {
+          Ki = ((float)encoder.getCount())/PID_ENCODER_RESOLUTION;
+          myPID.SetTunings(Kp, Ki, Kd);
+        }
+        
+        break;
+      case 3:
+        if(previousState != 3){
+          encoder.setCount(((int)Kd * PID_ENCODER_RESOLUTION));
+        }
+        if (Kd != (((float)encoder.getCount())/PID_ENCODER_RESOLUTION)) {
+          Kd = ((float)encoder.getCount())/PID_ENCODER_RESOLUTION;
+          myPID.SetTunings(Kp, Ki, Kd);
+        }
+        break;
+      case 4:
+         if(previousState != 4){
+          encoder.setCount(((int)calibration * 20.0));
+        }
+        calibration = ((float)encoder.getCount())/20.0;
+        break;
+    }
+    previousState = state;
+    input = steinhart;
+    myPID.Compute();
+    
     updateDisplay();
   }
-  if (millis() - windowStartTime > WindowSize)
-  { //time to shift the Relay Window
+  unsigned long curTime = millis();
+  if (curTime - windowStartTime > WindowSize) {
     windowStartTime += WindowSize;
   }
 //  Serial.println("setpoint*(WindowSize/100)="+String(setpoint*(WindowSize/100))+"millis-windowstarttime="+String(millis() - windowStartTime));
-  if (millis() - windowStartTime < output * (WindowSize/100)) digitalWrite(SSR_PIN, HIGH);
+  if (curTime - windowStartTime < output * (WindowSize/100)) digitalWrite(SSR_PIN, HIGH);
   else digitalWrite(SSR_PIN, LOW);
 }
