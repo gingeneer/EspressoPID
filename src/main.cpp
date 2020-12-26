e0f339fca45744a5bfdef72b132b25ac613d1ab0
Lukas Gygi <lukas.gygi@gmail.com>
2020-12-26 23:25:46 +0100

    fucking seems to work

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@
 #define THERMISTOR_PIN 34 // VP pin in my case
 #define SSR_PIN 13
 #define ENCODER_BUTTON_PIN 22
+#define BACK_BUTTON_PIN 14
 #define ENCODER_CLK 21
 #define ENCODER_DT 19
 // resistance at 25 degrees C
@@ -33,14 +34,16 @@
 // the value of the 'other' resistor
 #define SERIESRESISTOR 99000
 // how many speps per degree
-#define OFFSET_ENCODER_RESOLUTION 2.0
+#define SETPOINT_ENCODER_RESOLUTION 2.0
 #define PID_ENCODER_RESOLUTION 100.0
 
 #define EEPROM_SIZE 1000
 int samples[NUMSAMPLES];
 
-uint16_t tempArray[TFT_WIDTH];
-uint8_t outputArray[TFT_WIDTH];
+// uint16_t tempArray[TFT_WIDTH];
+// uint8_t outputArray[TFT_WIDTH];
+uint8_t previousTempY = 0;
+uint8_t previousDutyY = 40;
 
 ESP32Encoder encoder;
 
@@ -48,7 +51,9 @@ TFT_eSPI tft = TFT_eSPI(); // Invoke library, pins defined in User_Setup.h
 
 //Define Variables we'll be connecting to PID
 double setpoint = 105, input, output;
-float calibration = -2.3;
+double calibration = -2.3;
+//offset for display (what boiler temp equals which water temp)
+double temp_offset = 0;
 //Specify the links and initial tuning parameters
 double Kp = 2, Ki = 5, Kd = 1;
 PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
@@ -58,16 +63,19 @@ unsigned long windowStartTime;
 unsigned long shotTimerStart;
 unsigned long shotTimerStop;
 bool timerRunning;
+bool initGraph = false;
 
 uint8_t menu = 0;
 uint8_t state;
 uint8_t previousState = 0;
 uint8_t previousMenu = 0;
-uint8_t arrayIndex = 0;
+uint8_t graphX = 0;
 uint8_t selection = 0;
 bool longPressPossible = false;
 
-Bounce bounce = Bounce();
+Bounce encoderBounce = Bounce();
+Bounce backButtonBounce = Bounce();
+
 
 void saveValues()
 {
@@ -86,6 +94,87 @@ void saveValues()
     EEPROM.commit();
 }
 
+void updateGraph(float temp, float dutycycle, bool initialize)
+{
+    if (menu != 0)
+        return;
+    // min/max temp
+    const uint8_t min = 80;
+    const uint8_t max = 120;
+    // height of graphing area
+    const uint8_t height = 120;
+    // how many pixel per temp
+    float scale = float(height) / (float(max) - min);
+    uint8_t nextIndex = (graphX + 1) % TFT_WIDTH;
+    uint8_t tempY = TFT_HEIGHT - uint8_t(((temp - min) * scale + 0.5));
+    uint8_t dutyCycleY = TFT_HEIGHT - uint8_t(dutycycle * (float(height) / 100.0f) + 0.5);
+    if (dutyCycleY == TFT_HEIGHT)
+        dutyCycleY = TFT_HEIGHT - 1;
+    // draw the graph from back to front
+    // legend
+    uint8_t len = tft.textWidth("100", 1);
+    tft.drawFastVLine(graphX, TFT_HEIGHT - height - 4, height + 4, TFT_BLACK);
+    tft.drawPixel(graphX, TFT_HEIGHT - int((90 - min) * scale + 0.5), TFT_DARKGREY);
+    tft.drawPixel(graphX, TFT_HEIGHT - int((110 - min) * scale + 0.5), TFT_DARKGREY);
+    if (initialize)
+    {
+        tft.drawFastHLine(0, TFT_HEIGHT - int((90 - min) * scale + 0.5), TFT_WIDTH, TFT_DARKGREY);
+        tft.drawFastHLine(len, TFT_HEIGHT - int((100 - min) * scale + 0.5), TFT_WIDTH, TFT_DARKGREY);
+        tft.drawFastHLine(0, TFT_HEIGHT - int((110 - min) * scale + 0.5), TFT_WIDTH, TFT_DARKGREY);
+        tft.drawFastHLine(len, TFT_HEIGHT - int((max - min) * scale + 0.5), TFT_WIDTH, TFT_DARKGREY);
+    }
+    if (graphX <= len || initialize)
+    {
+        tft.setTextDatum(BL_DATUM);
+        tft.setTextFont(1);
+        tft.setTextSize(1);
+        tft.setTextColor(TFT_DARKGREY);
+        tft.drawNumber(min, 0, TFT_HEIGHT);
+        tft.setTextDatum(CL_DATUM);
+        tft.drawNumber(100, 0, TFT_HEIGHT - int((100.0 - min) * scale + 0.5));
+        //tft.drawFastHLine(len, TFT_HEIGHT - int((100.0 - min) * scale + 0.5), TFT_WIDTH, TFT_DARKGREY);
+        tft.drawNumber(max, 0, TFT_HEIGHT - int(float(max - min) * scale + 0.5));
+        //tft.drawFastHLine(len, TFT_HEIGHT - int(float(max - min) * scale + 0.5), TFT_WIDTH, TFT_DARKGREY);
+        // actual graph
+    }
+    else
+    {
+        tft.drawPixel(graphX, TFT_HEIGHT - int((100 - min) * scale + 0.5), TFT_DARKGREY);
+        tft.drawPixel(graphX, TFT_HEIGHT - int((max - min) * scale + 0.5), TFT_DARKGREY);
+    }
+
+    if (dutyCycleY > previousDutyY)
+    {
+        tft.drawFastVLine(graphX, previousDutyY, dutyCycleY - previousDutyY + 1, TFT_GREEN);
+    }
+    else if (dutyCycleY < previousDutyY)
+    {
+        tft.drawFastVLine(graphX, dutyCycleY, previousDutyY - dutyCycleY + 1, TFT_GREEN);
+    }
+    else
+    {
+        tft.drawPixel(graphX, dutyCycleY, TFT_GREEN);
+    }
+    if (temp >= min && temp <= max)
+    {
+        if (tempY > previousTempY)
+        {
+            tft.drawFastVLine(graphX, previousTempY, tempY - previousTempY + 1, TFT_RED);
+        }
+        else if (tempY < previousTempY)
+        {
+            tft.drawFastVLine(graphX, tempY, previousTempY - tempY + 1, TFT_RED);
+        }
+        else
+        {
+            tft.drawPixel(graphX, tempY, TFT_RED);
+        }
+    }
+    tft.drawFastVLine(nextIndex, TFT_HEIGHT - height - 4, height + 4, TFT_WHITE);
+    previousDutyY = dutyCycleY;
+    previousTempY = tempY;
+}
+
 // update the display output
 void updateDisplay()
 {
@@ -97,25 +186,28 @@ void updateDisplay()
     int uptime_mins = curTime / (1000 * 60);
     int uptime_secs = (curTime / (1000)) % 60;
     tft.printf("%3d:%02d", uptime_mins, uptime_secs);
-    tft.setTextDatum(TR_DATUM); // top right
-    tft.drawFloat(setpoint, 1, TFT_WIDTH, 0);
+    tft.setTextDatum(TR_DATUM); // top right]
+    char buf[6];
+    snprintf(buf, 6, "%5.1f", setpoint);
+    tft.drawString(buf, TFT_WIDTH, 0);
     tft.setTextDatum(TC_DATUM); // top center
-    char buf[5];
-    snprintf(buf, 5, "%d%%", (int)output);
+    snprintf(buf, 5, "%3d%%", (int)output);
     tft.drawString(buf, TFT_WIDTH / 2, 0);
     tft.drawFastHLine(0, tft.fontHeight(1) + 1, TFT_WIDTH, TFT_ORANGE);
+
+    // degrees
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.setTextDatum(TL_DATUM);
+    tft.setTextFont(2);
+    uint8_t len = tft.drawFloat(input + temp_offset, 1, 0, 12);
+    tft.setTextFont(1);
+    tft.drawString("o", len, 12);
 
     switch (menu)
     {
     case 0:
         // cant do that...
         // tft.fillRect(0, 12, TFT_WIDTH, TFT_HEIGHT, TFT_BLACK);
-        tft.setTextColor(TFT_WHITE, TFT_BLACK);
-        tft.setTextDatum(TL_DATUM);
-        tft.setTextFont(2);
-        uint8_t len = tft.drawFloat(input, 1, 0, 12);
-        tft.setTextFont(1);
-        tft.drawString("o", len, 12);
         tft.setTextDatum(TR_DATUM);
         char buf[7];
         switch (state)
@@ -135,35 +227,55 @@ void updateDisplay()
         }
         tft.setTextFont(2);
         tft.drawString(buf, TFT_WIDTH, 12);
-        // graph part
-        const int min = 70;
-        const int max = 160;
-        // const int height = 90;
-        tft.setTextDatum(BL_DATUM);
+        break;
+    case 1:
+        // show all the values, the selection is controlled by moveselecion in the FSM
+        tft.setTextColor(TFT_WHITE, TFT_BLACK);
         tft.setTextFont(1);
-        tft.setTextSize(1);
-        tft.setTextColor(TFT_DARKGREY);
-        tft.drawNumber(min, 0, TFT_HEIGHT);
-        tft.setTextDatum(CL_DATUM);
-        len = tft.drawNumber(100, 0, TFT_HEIGHT - 100 + min);
-        tft.drawFastHLine(len, TFT_HEIGHT - 100 + min, TFT_WIDTH, TFT_DARKGREY);
-        len = tft.drawNumber(max, 0, TFT_HEIGHT - max + min);
-        tft.drawFastHLine(len, TFT_HEIGHT - max + min, TFT_WIDTH, TFT_DARKGREY);
-        // this is too slow
-        // int index = arrayIndex;
-        // uint8_t previousDuty = outputArray[0];
-        // uint8_t previousTemp = tempArray[0];
-        // for (int i = 0; i < TFT_WIDTH; i++)
-        // {
-        //     uint8_t temp = TFT_HEIGHT - ((tempArray[index] / 100) - 70);
-        //     uint8_t dutyCycle = TFT_HEIGHT - outputArray[index];
-        //     tft.drawFastVLine(i, previousTemp, temp, TFT_RED);
-        //     tft.drawFastVLine(i, previousDuty, dutyCycle, TFT_GREEN);
-        //     index = (index + 1) % TFT_WIDTH;
-        //     previousDuty = dutyCycle;
-        //     previousTemp = temp;
-        // }
+        tft.setCursor(10, 40);
+        tft.printf("setpoint: %7.2f", setpoint);
+        tft.setCursor(10, 50);
+        tft.printf("P: %6.3f", Kp);
+        tft.setCursor(10, 60);
+        tft.printf("I: %6.3f", Ki);
+        tft.setCursor(10, 70);
+        tft.printf("D: %6.3f", Kd);
+        tft.setCursor(10, 80);
+        tft.printf("Therm. cal:%7.2f", calibration);
+        tft.setCursor(10, 90);
+        tft.printf("T offset:%4.1f", temp_offset);
+        // uint8_t fontheight = tft.fontHeight(1);
+        if (state == 6)
+        {
+           
+            tft.setCursor(10, 110);
+            tft.setTextColor(TFT_ORANGE, TFT_BLACK);
+            tft.print("save to EEPROM?");
+            
+            tft.setCursor(10, 120);
+            if ((encoder.getCount() & 0b1) == 0)
+                tft.setTextColor(TFT_BLACK, TFT_GREEN);
+            else
+                tft.setTextColor(TFT_WHITE, TFT_BLACK);
+            tft.print("Yes");
+            if ((encoder.getCount() & 0b1) == 1)
+                tft.setTextColor(TFT_BLACK, TFT_RED);
+            else
+                tft.setTextColor(TFT_WHITE, TFT_BLACK);
+            tft.setCursor(60, 120);
+            tft.print("No");
+        }
+        break;
     }
+}
+
+void moveSelection(uint8_t from, uint8_t to, bool visible)
+{
+    // 0-5: x: 2, y: 42+x*10, cursor 6x6 rect
+    tft.fillRect(2, 40 + from * 10, 7, 7, TFT_BLACK);
+    if (visible)
+        tft.fillRect(2, 40 + to * 10, 7, 7, TFT_RED);
+
 }
 
 void setup(void)
@@ -175,21 +287,23 @@ void setup(void)
     tft.init();
     tft.setRotation(2);
     tft.fillScreen(TFT_BLACK);
-    // We can now plot text on screen using the "print" class
-    // tft.println("Hello World!");
+    encoderBounce.attach(ENCODER_BUTTON_PIN, INPUT_PULLUP);
+    encoderBounce.interval(25);
+    backButtonBounce.attach(BACK_BUTTON_PIN, INPUT_PULLUP);
+    backButtonBounce.interval(25);
+    delay(100);
     // initialize values from EEPROM
+    backButtonBounce.update();
     if (!EEPROM.begin(EEPROM_SIZE))
     {
         tft.println("faled to initialise EEPROM");
         delay(2000);
     }
-    else
+    // hold back button on startup to skip load from EEPROM
+    else if (backButtonBounce.read() == HIGH)
     {
         // load the values from EEPROM
-        for (int i = 0; i < 5; i += sizeof(float))
-        {
-            printf("EEPROM %d: %f\n", i, EEPROM.readFloat(i));
-        }
+
         int addr = 0;
         setpoint = EEPROM.readFloat(addr);
         addr += sizeof(float);
@@ -211,31 +325,40 @@ void setup(void)
     // Enable the weak pull up resistors
     ESP32Encoder::useInternalWeakPullResistors = UP;
     // Attache pins for use as encoder pins
-    encoder.attachHalfQuad(ENCODER_CLK, ENCODER_DT);
-    encoder.setCount(setpoint * OFFSET_ENCODER_RESOLUTION);
-    bounce.attach(ENCODER_BUTTON_PIN, INPUT_PULLUP);
-    bounce.interval(25);
+    encoder.attachHalfQuad(ENCODER_DT, ENCODER_CLK);
+    encoder.setCount(setpoint * SETPOINT_ENCODER_RESOLUTION);
+   
+    initGraph = true;
 }
 
 void loop(void)
 {
     unsigned long looptime = millis();
-    bounce.update();
+    encoderBounce.update();
+    backButtonBounce.update();
     bool selectPressed = false;
-    // bool backPressed = false;
+    bool backPressed = false;
     bool longSelect = false;
     bool update = false;
-    if (bounce.changed())
+    if (encoderBounce.changed())
     {
-        if (bounce.read() == 0)
+        if (encoderBounce.read() == 0)
         {
             update = true;
             longPressPossible = true;
             selectPressed = true;
         }
     }
+    if (backButtonBounce.changed())
+    {
+        if (backButtonBounce.read() == 0)
+        {
+            update=true;
+            backPressed = true;
+        }
+    }
     // check for longpress
-    if (bounce.read() == 0 && bounce.duration() > 3000 && longPressPossible)
+    if (encoderBounce.read() == 0 && encoderBounce.duration() > 3000 && longPressPossible)
     {
         longSelect = true;
         longPressPossible = false;
@@ -247,7 +370,7 @@ void loop(void)
         switch (menu)
         {
         case 0:
-            setpoint = ((float)encoder.getCount()) / OFFSET_ENCODER_RESOLUTION;
+            setpoint = ((float)encoder.getCount()) / SETPOINT_ENCODER_RESOLUTION;
             switch (state)
             {
             case 0:
@@ -275,9 +398,11 @@ void loop(void)
             }
             if (longSelect)
             {
+                tft.fillScreen(TFT_BLACK);
+                moveSelection(0, 0, true);
                 menu = 1;
                 state = 0;
-                encoder.setCount(setpoint * OFFSET_ENCODER_RESOLUTION);
+                encoder.setCount(setpoint * SETPOINT_ENCODER_RESOLUTION);
             }
             break;
         case 1:
@@ -286,15 +411,23 @@ void loop(void)
             {
             case 0:
                 // roatary encoder controlls the setpoint
-                if (previousState != 0 || previousMenu != menu)
-                {
-                    encoder.setCount(setpoint * OFFSET_ENCODER_RESOLUTION);
-                }
-                setpoint = ((float)encoder.getCount()) / OFFSET_ENCODER_RESOLUTION;
+                // if (previousState != 0 || previousMenu != menu)
+                // {
+                //     encoder.setCount(setpoint * SETPOINT_ENCODER_RESOLUTION);
+                // }
+                setpoint = ((float)encoder.getCount()) / SETPOINT_ENCODER_RESOLUTION;
                 if (selectPressed)
                 {
                     state = 1;
                     encoder.setCount(Kp * PID_ENCODER_RESOLUTION);
+                    moveSelection(0, 1, true);
+
+                }
+                if (backPressed)
+                {
+                    state = 6;
+                    encoder.setCount(0);
+                    moveSelection(0, 0, false);
                 }
                 break;
             case 1:
@@ -307,6 +440,13 @@ void loop(void)
                 {
                     state = 2;
                     encoder.setCount(Ki * PID_ENCODER_RESOLUTION);
+                    moveSelection(1, 2, true);
+                }
+                if (backPressed)
+                {
+                    state = 6;
+                    encoder.setCount(0);
+                    moveSelection(0, 0, false);
                 }
                 break;
             case 2:
@@ -319,6 +459,13 @@ void loop(void)
                 {
                     state = 3;
                     encoder.setCount(Kd * PID_ENCODER_RESOLUTION);
+                    moveSelection(2, 3, true);
+                }
+                if (backPressed)
+                {
+                    state = 6;
+                    encoder.setCount(0);
+                    moveSelection(0, 0, false);
                 }
                 break;
             case 3:
@@ -331,23 +478,67 @@ void loop(void)
                 {
                     state = 4;
                     encoder.setCount(calibration * 20.0);
+                    moveSelection(3, 4, true);
+                }
+                if (backPressed)
+                {
+                    state = 6;
+                    encoder.setCount(0);
+                    moveSelection(0, 0, false);
                 }
                 break;
             case 4:
                 calibration = ((float)encoder.getCount()) / 20.0;
                 if (selectPressed)
                 {
-                    encoder.setCount(setpoint * OFFSET_ENCODER_RESOLUTION);
-                    state = 0;
+                    encoder.setCount(temp_offset * 10.0);
+                    state = 5;
+                    moveSelection(4, 5, true);
+                }
+                if (backPressed)
+                {
+                    state = 6;
+                    encoder.setCount(0);
+                    moveSelection(0, 0, false);
                 }
                 break;
-            }
-            if (longSelect)
-            {
-                // saveValues();
-                menu = 0;
-                state = 0;
-                encoder.setCount(setpoint * OFFSET_ENCODER_RESOLUTION);
+            case 5:
+                temp_offset = ((double)encoder.getCount()) / 10.0;
+                if (selectPressed)
+                {
+                    encoder.setCount(setpoint * SETPOINT_ENCODER_RESOLUTION);
+                    state = 0;
+                    moveSelection(5, 0, true);
+                }
+                if (backPressed)
+                {
+                    state = 6;
+                    encoder.setCount(0);
+                    moveSelection(0, 0, false);
+                }
+                break;
+            case 6:
+                if (selectPressed)
+                {
+                    if ((encoder.getCount() & 0b1) == 0)
+                    {
+                        saveValues();
+                    }
+                    menu = 0;
+                    state = 0;
+                    encoder.setCount(setpoint * SETPOINT_ENCODER_RESOLUTION);
+                    tft.fillScreen(TFT_BLACK);
+                    initGraph = true;
+                    graphX = 0;
+                }
+                if (backPressed)
+                {
+                    encoder.setCount(setpoint * SETPOINT_ENCODER_RESOLUTION);
+                    state = 0;
+                    tft.fillScreen(TFT_BLACK);
+                    moveSelection(0, 0, true);
+                }
+                break;
             }
             break;
         }
@@ -382,22 +573,17 @@ void loop(void)
         steinhart = 1.0 / steinhart;                      // Invert
         steinhart -= 273.15;                              // convert absolute temp to C
         steinhart += calibration;
-        input = steinhart;
+        input = steinhart + temp_offset;
         myPID.Compute();
         updateDisplay();
     }
     // save to the array for the graphics
-    if (looptime % 500 == 0)
+    if (looptime % 1000 == 0)
     {
-        uint8_t temp = (uint8_t)(input - 80);
-        if (input - 80 < 0)
-        {
-            temp = 0;
-        }
-        tempArray[arrayIndex] = (uint16_t)(temp * 100);
-        outputArray[arrayIndex] = (uint8_t)(output);
-        arrayIndex = (arrayIndex + 1) % TFT_WIDTH;
+        graphX = (graphX + 1) % TFT_WIDTH;
         Serial.println(String(input) + ", " + String(output));
+        updateGraph(input + temp_offset, output, initGraph);
+        initGraph = false;
     }
 
     // output logic for the SSR
